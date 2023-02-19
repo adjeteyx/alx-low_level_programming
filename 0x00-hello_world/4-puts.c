97bc7c5e1e71b70b975b56988ae1fe8e10d18535
adjeteyx <adjeteyx>
2023-02-19 02:48:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: o
- */
+*main -  entry point
+*return : always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
