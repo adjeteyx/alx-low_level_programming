1137123491e481dde59e121ae453889f20b542c7
adjeteyx <adjeteyx>
2023-02-27 04:39:27 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * reset_to_98 - function that a pointer to an int as parameter updates
+  *
+  * @n: input
+  * Return: n
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
