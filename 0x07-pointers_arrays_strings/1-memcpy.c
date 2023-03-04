1833ad88915fc664c8b82f3dc40024c77bfa74ad
adjeteyx <adjeteyx>
2023-03-04 05:36:39 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r - 0;
-	int i - n;
+	int r = 0;
+	int i = n;
 
-	for c; r < i; r++)
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
