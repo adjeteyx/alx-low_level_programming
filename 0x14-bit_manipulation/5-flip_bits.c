33bb3095a9e57179462c4d1917476a6aee4f8a07
adjeteyx <adjeteyx>
2023-04-11 06:42:21 -0700

    5-flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+/**
+ * flip_bits - bits to change
+ * to get from one number to another
+ * @n: number 1
+ * @m: number 2
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
